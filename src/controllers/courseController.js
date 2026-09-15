8c76aa225853f22376b9224dd584799349658eb4
Daizy <daizyleticianyuyubunridzem>
2026-09-15 19:43:16 +0100

    Implement course creation with PRG

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -9,6 +9,77 @@ const getCourses = async (req, res, next) => {
     }
 };
 
+const showCreateForm = (req, res) => {
+    res.render('courses/new');
+};
+
+const createCourse = async (req, res, next) => {
+    try {
+        const { courseName, description, duration, instructor, maxStudents } = req.body;
+        await Course.create({ courseName, description, duration, instructor, maxStudents });
+
+        res.redirect('/courses');
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getCourse = async (req, res, next) => {
+    try {
+        const course = await Course.findById(req.params.id);
+        res.render('courses/show', { course });
+
+    } catch (error) {
+        next(error);
+    }
+};
+const showEditForm = async (req, res, next) => {
+    try {
+        const course = await Course.findById(req.params.id);
+        res.render('courses/edit', { course });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateCourse = async (req, res, next) => {
+    try {
+        const { courseName, description, duration, instructor, maxStudents } = req.body;
+        await Course.findByIdAndUpdate( req.params.id,
+            { courseName,
+                description,
+                duration,
+                instructor,
+                maxStudents
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+        res.redirect('/courses');
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteCourse = async (req, res, next) => {
+    try {
+        await Course.findByIdAndDelete(req.params.id);
+        res.redirect('/courses');
+    } catch (error) {
+        next(error);
+    }
+};
+
+
 module.exports = {
-    getCourses
+    getCourses,
+    showCreateForm,
+    createCourse,
+    getCourse,
+    showEditForm,
+    updateCourse,
+    deleteCourse
 };
--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 router.get('/', courseController.getCourses);
+router.get('/new', courseController.showCreateForm);
+router.post('/', courseController.createCourse);
+router.get('/:id/edit', courseController.showEditForm);
+router.post('/:id/edit', courseController.updateCourse);
+router.post('/:id/delete', courseController.deleteCourse);
+router.get('/:id', courseController.getCourse);
 
 module.exports = router;
