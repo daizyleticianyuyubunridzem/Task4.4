3a9df81799101ee93620d42252498b397b92aae7
Daizy <daizyleticianyuyubunridzem>
2026-09-15 07:42:04 +0100

    Set up Express and EJS

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Course catalogue application');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+});
